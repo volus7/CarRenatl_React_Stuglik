8618e20c7843e0d1463d88612677c707bf671fa3
vektor <volus7@o2.pl>
2024-03-26 15:14:37 +0100

    tests

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import { Aumto } from './classes/Automo';
 
 
 function App() {
-  const array = [
-  new Aumto("Negrobus","super auto polecam 11/10", 5, "N","Makumba123"),
-   new Aumto("Dorożka","Moc: 2 konie (nie) mechaniczne", 2, "śmierdzi koniem", "bmesdhvkb"),
-   new Aumto("Auto Freda","styl retro", 4, "^_^", "barney_rubble"),
-   new Aumto("Słoń bojowy","kto by nie chciał słonia bojowego", 5, "polecam", "Hannibal")  ]
+  // const array = [
+  // new Aumto("Negrobus","super auto polecam 11/10", 5, "N","Makumba123"),
+  //  new Aumto("Dorożka","Moc: 2 konie (nie) mechaniczne", 2, "śmierdzi koniem", "bmesdhvkb"),
+  //  new Aumto("Auto Freda","styl retro", 4, "^_^", "barney_rubble"),
+  //  new Aumto("Słoń bojowy","kto by nie chciał słonia bojowego", 5, "polecam", "Hannibal")  ]
   return (
     <>
-    {
+    {/* {
       array.map(e => (<div className="car_card" key={e.name}>
       <div style={{"float":"left"}} className="car_info">
 
@@ -39,8 +39,7 @@ function App() {
      {
       array.map( autmo => <Props props={autmo} />)
      }
-         <hr />
-
+         <hr /> */}
       <Test />
     </>
   )
--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,31 +1,115 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 
-export const Test = () => {
-    const [car_names, set_car_names] = useState([]);
+// export const Test = () => {
+//     const [car_names, set_car_names] = useState([]);
 
-    const get_data = async() => {
-        const req = await fetch('../../API/fetch.php');
-        const res = await req.json();
-        console.log(res);
-    }
+//     const get_data = async() => {
+//         const req = await fetch('../../API/fetch.php');
+//         const res = await req.json();
+//         console.log(res);
+//     }
 
-    useEffect(() => {
-        set_car_names(['auto 1', 'auto 2']);
-        get_data();
-    }, []);
+//     useEffect(() => {
+//         set_car_names(['auto 1', 'auto 2']);
+//         get_data();
+//     }, []);
 
    
 
-    return (
-        <div className='cars'>
-            {
-                car_names.map(car_name => {
-                    return <div>
-                        {car_name}
-                    </div>
-                })
-            }
+//     return (
+//         <div className='cars'>
+//             {
+//                 car_names.map(car_name => {
+//                     return <div>
+//                         {car_name}
+//                     </div>
+//                 })
+//             }
+//         </div>
+//     );
+// }
+// export default Test;
+
+import React, { useState, useEffect } from 'react';
+
+export const Test = () => {
+ const [brands, setBrands] = useState([]);
+ 
+ useEffect(() => {
+     const fetchData = async () => {
+       const response = await fetch('../../API/fetch.php');
+       const data = await response.json();
+    //    console.log(data);
+       setBrands(data);
+     };
+     fetchData();
+ }, []);
+
+ return (
+    <div className='test'>
+        <p>jestem w comps</p>
+      {brands.map((brand, index) => (
+        <div key={index}>
+          <p>Brand ID: {brand.brand_id}</p>
+          <p>Brand Name: {brand.brand_name}</p>
         </div>
-    );
-}
+      ))}
+    </div>
+ );
+};
+
 export default Test;
+
+
+// import React, { useState, useEffect } from 'react';
+
+// export const Test = () => {
+//  const [brands, setBrands] = useState([]);
+//  const [isLoading, setIsLoading] = useState(true);
+//  const [error, setError] = useState(null);
+
+//  const fetchData = async () => {
+//     try {
+//     fetch('../../API/fetch.php')
+//         .then(response => response.text())
+//         .then(text => console.log(text))
+//         .catch(error => console.error('Error:', error));
+
+//       const response = await fetch('../../API/fetch.php');
+//       if (!response.ok) {
+//         throw new Error('Network response was not ok');
+//       }
+//       const data = await response.json();
+//       setBrands(data);
+//       setIsLoading(false);
+//     } catch (error) {
+//       setError(error.message);
+//       setIsLoading(false);
+//     }
+//  };
+
+//  useEffect(() => {
+//     fetchData();
+//  }, []);
+
+//  if (isLoading) {
+//     return <div>Loading...</div>;
+//  }
+
+//  if (error) {
+//     return <div>Error: {error}</div>;
+//  }
+
+//  return (
+//     <div>
+//       {brands.map((brand) => (
+//         <div key={brand.brand_id}>
+//           <p>Brand ID: {brand.brand_id}</p>
+//           <p>Brand Name: {brand.brand_name}</p>
+//         </div>
+//       ))}
+//     </div>
+//  );
+// };
+
+// export default Test;
